df4c78d0280a64ebd3b3c5d7e52fd84a17d2f0a4
madLifeZPC <zhaopc21@163.com>
2016-11-07 10:24:35 +0800

    simple backend

---
+++ ca3/week05_ca/src/java/edu/nus/iss/ca3/entity/Delivery.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.nus.iss.ca3.entity;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author madLife
+ */
+@Entity
+@Table(name = "delivery")
+public class Delivery {
+
+    @Id
+    @Column(name = "pkg_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "create_date")
+    private Timestamp date;
+
+    @OneToOne(mappedBy = "delivery")
+    private Pod pod;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public Pod getPod() {
+        return pod;
+    }
+
+    public void setPod(Pod pod) {
+        this.pod = pod;
+    }
+    
+    
+
+}
---
+++ ca3/week05_ca/src/java/edu/nus/iss/ca3/entity/Pod.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.nus.iss.ca3.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author madLife
+ */
+public class Pod {
+    
+    @Id
+    @Column(name = "pod_id")
+    private int id;
+
+    @Column(name = "note")
+    private String note;
+
+    @Lob 
+    @Basic(fetch=FetchType.LAZY)
+    @Column(name = "image")
+    private byte[] image;
+            
+    @Column(name = "ack_id")
+    private String ack_id;
+
+    @Column(name = "delivery_date")
+    private Timestamp delivery_date;
+
+    @OneToOne
+    private Delivery delivery;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getAck_id() {
+        return ack_id;
+    }
+
+    public void setAck_id(String ack_id) {
+        this.ack_id = ack_id;
+    }
+
+    public Timestamp getDelivery_date() {
+        return delivery_date;
+    }
+
+    public void setDelivery_date(Timestamp delivery_date) {
+        this.delivery_date = delivery_date;
+    }
+
+    public Delivery getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+    }
+    
+    
+    
+    
+}
